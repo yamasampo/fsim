1d55e21e86348fc67cc67b72dd057616972bcf8b
Haruka Yamashita <hyamashita@nig.ac.jp>
2022-04-26 15:22:01 +0900

    Add functions to compute the probability of obtaining number of a given number of mutant offsprings at the next generation.

--- fsim/theory.py
+++ fsim/theory.py
@@ -1,4 +1,6 @@
 import math
+from multiprocessing.sharedctypes import Value
+from pandas import value_counts
 from scipy.integrate import quad
 from typing import Union
 
@@ -31,7 +33,7 @@ def time_to_fixation_haploid(
     
     # Adjust parameters to fit the formula.
     # Kimura and Ohta (1969) considered the mutant gene A1 has the selective 
-    # advantage s/2 over its allel A1. But the input argument s itself is 
+    # advantage s/2 over its allele A1. But the input argument s itself is 
     # defined as the selective advantage. 
     s = 2 * s
 
@@ -92,3 +94,73 @@ def fixation_probability_haploid(init_freq, ns):
     nume = 1 - math.exp(-2 * ns * init_freq)
     
     return nume / denom
+
+def prob_of_obtaining_given_number_of_neutral_mutants_next_gen(
+        pop_size:int,
+        parental_mut_num:int,
+        offspring_mut_num:int
+        ):
+    """ Returns a probability of obtaining a given number of mutant alleles at 
+    the next generation, given a number of mutant alleles in a parental 
+    population. This function considers haploid organism (i.e., population size 
+    is equal to a total number of alleles in the population).
+    
+    Assumption
+    ----------
+    1. No mutation: 
+        A new mutation does not occur.
+    2. Random mating: 
+        Each individual has an equal probability of becoming a parent. 
+    3. Perfect reproduction: 
+        All the parents successfully produce offsprings.
+    4. Constant population size: 
+        The population size does not change over time. 
+        parental pop size = offspring pop size = pop_size
+    5. Mutually exclusive configurations
+        Observing different types of configurations (order of mutant and 
+        wildtype alleles) do not occur simultaneously. 
+    
+    Parameters
+    ----------
+    pop_size: int
+        A number of individuals in a population.
+    parental_mut_num: int
+        A number of mutant alleles in the parent population.
+    offspring_mut_num: int
+        A number of mutant alleles in the next (offspring) population.
+
+    """
+    # Probability of "mating" (haveing an opportunity to become a parent) 
+    # for a mutant individual at t generation. 
+    #   Assumption 1: no mutation
+    #   Assumption 2: random mating (1 / pop_size x parental_mut_num)
+    pt_mating_mut = parental_mut_num / pop_size
+
+    # Probability of reproduction (making an offspring) for the parent
+    #   Assumption 3: perfect reproduction
+    pt_mating_success_mut = pt_mating_mut
+    pt_mating_success_wild = 1 - pt_mating_success_mut
+    
+    # Probability that mutant and wildtype individuals made offsprings in a 
+    # given order (configuration)
+    #   Assumption 2: Random mating
+    #   Assumption 3: Perfect reproduction
+    #   Assumption 4: Constant population size
+    pt_observing_one_config = \
+        pt_mating_success_mut ** offspring_mut_num \
+        * pt_mating_success_wild ** (pop_size - offspring_mut_num)
+    
+    # A total number of configurations
+    #   Assumption 4: Constant population size
+    #   Assumption 5: Mutually exclusive configurations
+    total_configs = number_of_combinations(pop_size, offspring_mut_num)
+    return pt_observing_one_config * total_configs
+
+def number_of_combinations(n, r):
+    if r == 0:
+        return 1
+    elif r == n:
+        return 1
+
+    return math.factorial(n) / (math.factorial(n - r) * math.factorial(r))
+
